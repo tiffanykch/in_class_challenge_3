0b6ad2bda6896592fbacb239917c6c0ab7bc669e
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-02-13 16:16:49 -0500

    Completed Task 6 to Task 8

--- cc_example.js
+++ cc_example.js
@@ -1,4 +1,4 @@
-// Task 5
+// TASK 5
 
 let orders = [
     {id: 101, customer: "Alice", total: 300},
@@ -10,4 +10,57 @@ function findOrder(orders, orderId) {
     return orders.find(order => order.id === orderId);
 }
 
-console.log(findOrder(orders, 102));
+// console.log(findOrder(orders, 102));
+
+// TASK 6 
+
+let inventory = {
+    items: [],
+    addItem(name, quantity) {
+        this.items.push({ name, quantity });
+    },
+    removeLatestItem() {
+        this.items.pop()
+    },
+    removeFirstItem(){
+        this.items.shift();
+    }
+
+};
+
+inventory.addItem("Monitor", 5);
+inventory.addItem("Keyboard", 10);
+inventory.addItem("Mouse", 3)
+// console.log(JSON.stringify(inventory.items));
+
+// inventory.removeLatestItem()
+// console.log(JSON.stringify(inventory.items));
+
+// inventory.removeFirstItem()
+// console.log(JSON.stringify(inventory.items));
+
+// TASK 7
+
+let employees = [
+    {name: "Alice", position: "Developer", salary: 70000},
+    {name: "Bob", position: "Designer", salary: 60000},
+    {name: "Charlie", postion: "Manager", salary: 90000}
+]
+
+function findEmployee(employees, name) {
+    return employees.find(employee => employee.name === name);
+}
+
+console.log(findEmployee(employees, "Bob"));
+
+// TASK 8
+
+let orders1 = [{id: 1, customer: " Alice"}, {id: 2, customer: "Bob"}]
+let orders2 = [{id: 3, customer: "Charlie"}, {id: 4, customer: "David"}]
+
+
+function mergeOrders(orderA, orderB) {
+    return [...orderA, ...orderB];
+}
+
+console.log(mergeOrders(orders1, orders2))
